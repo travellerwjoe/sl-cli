b453e57ad21cbdcb123a715b2fa8d6cbf40d5ba6
joe.wu <traveller_wjoe@sina.com>
2017-02-17 21:55:36 +0800

    修复未init执行watch命令时报错

--- command/watch.js
+++ command/watch.js
@@ -5,8 +5,8 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = () => {
-    const src = config.publish.src;
-    console.log('Watch the file or directory changes...');
+    const src = config.publish ? config.publish.src : [];
+    src.length ? console.log('Watch the file or directory changes...') : console.log('Please execute command `shanglv init` or `shanglv config -s <src>` first.');
     for (let thisSrc of src) {
         fs.watch(thisSrc, {
             recursive: true
